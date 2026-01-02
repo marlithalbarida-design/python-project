d9151058e3e93e7486f5b354747588d954550592
marlithalbarida-design <marlith.albarida@evsu.edu.ph>
2026-01-02 20:17:55 +0800

    add code for project

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+});
